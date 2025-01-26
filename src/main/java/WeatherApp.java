1f1cc610640ffe5e401198b19e5e1f0ac02cd083
ashfrazer <afrazer@cub.uca.edu>
2025-01-26 03:03:29 -0600

    Front-End and Controller skeletons done (may change later)

--- src/main/java/WeatherApp.java
+++ src/main/java/WeatherApp.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class WeatherApp {
     // FRONT-END
     // TO-DO:
     //  - Create GUI interface
+    private WeatherController weatherController;
+
+    public WeatherApp() {
+        WeatherScraper weatherScraper = new WeatherScraper();
+        this.weatherController = new WeatherController(weatherScraper, this);
+
+        // TO-DO: GUI LOGIC
+    }
+    public void updateWeatherUI(WeatherData weatherData) {
+        // TO-DO: UPDATE GUI LOGIC
+    }
+    public static void main(String[] args) {
+        new WeatherApp();
+    }
 }
--- src/main/java/WeatherController.java
+++ src/main/java/WeatherController.java
@@ -1,5 +1,14 @@
 public class WeatherController {
-    // BRIDGE
-    // TO-DO:
-    //  - Create a bridge between front-end and back-end
+    private WeatherScraper weatherScraper;
+
+    public WeatherController(WeatherScraper weatherScraper, WeatherApp weatherApp) {
+        this.weatherScraper = weatherScraper;
+    }
+
+    public void getWeather(String zipcode) {
+        // Create weatherData object that contains scraped data
+        WeatherData weatherData = weatherScraper.scrapeData(zipcode);
+
+        // TO-DO: Call GUI update method
+    }
 }
