da6c7b4041d1f0b4bdbede728c60f72658f49bdb
ashfrazer <afrazer@cub.uca.edu>
2025-01-26 02:29:03 -0600

    Implemented WeatherScraper, WeatherData Back-End classes. Added WeatherScraperTest. Added JUnit dependency.

--- src/main/java/WeatherData.java
+++ src/main/java/WeatherData.java
@@ -1,5 +1,56 @@
 public class WeatherData {
-    // BACK-END
-    // TO-DO:
-    //  - Will contain weather data
+    private String location;
+    private String temperature;
+    private String condition;
+    private String high;
+    private String low;
+
+    public WeatherData(String location, String temperature, String condition, String high, String low) {
+        this.location = location;
+        this.temperature = temperature;
+        this.condition = condition;
+        this.high = high;
+        this.low = low;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    public String getLocation() {
+        return location;
+    }
+    public void setTemperature(String temperature) {
+        this.temperature = temperature;
+    }
+    public String getTemperature() {
+        return temperature;
+    }
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+    public String getCondition() {
+        return condition;
+    }
+    public void setHigh(String high) {
+        this.high = high;
+    }
+    public String getHigh() {
+        return high;
+    }
+    public void setLow(String low) {
+        this.low = low;
+    }
+    public String getLow() {
+        return low;
+    }
+    @Override
+    public String toString() {
+        return String.format("""
+                Location: %s
+                Temperature: %s
+                Condition: %s
+                High: %s
+                Low: %s
+                """,
+                location, temperature, condition, high, low);
+    }
 }
--- src/main/java/WeatherScraper.java
+++ src/main/java/WeatherScraper.java
@@ -1,5 +1,63 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+
 public class WeatherScraper {
-    // BACK-END
-    // TO-DO:
-    //  - Fetches data from HTML
+
+    public WeatherData scrapeData(String zipcode) {
+        // Create the URL
+        String url = "https://weather.com/weather/today/l/" + zipcode;
+
+        // Initialize weather variables (in case scraping is unsuccessful)
+        String location = "UNKNOWN";
+        String temperature = "No data found.";
+        String condition = "No data found.";
+        String high = "No data found.";
+        String low = "No data found.";
+
+        Document doc = null;
+
+        try {
+            // Access the HTML page
+            doc = Jsoup.connect(url).get();
+
+            // Scrape the location (based on zipcode)
+            location = getElementText(doc.select("h1.CurrentConditions--location--yub4l").first());
+
+            // Scrape the current temperature
+            temperature = getElementText(doc.select("span.CurrentConditions--tempValue--zUBSz").first());
+
+            // Scrape the current conditions
+            condition = getElementText(doc.select("div.CurrentConditions--phraseValue---VS-k").first());
+
+            // Scrape the high temperature
+            high = getElementText(doc.select(
+                            "div.CurrentConditions--tempHiLoValue--Og9IG span[data-testid='TemperatureValue']")
+                    .first());
+
+            // Scrape the low temperature
+            low = getElementText(doc.select(
+                            "div.CurrentConditions--tempHiLoValue--Og9IG span[data-testid='TemperatureValue']")
+                    .last());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Create WeatherData object containing weather info
+        WeatherData weatherData = new WeatherData(location, temperature, condition, high, low);
+
+        // Return complete WeatherData object
+        return weatherData;
+    }
+    private String getElementText(Element element) {
+        if (element != null) {
+            return element.text();
+        }
+        else {
+            return "No data found.";
+        }
+    }
 }
